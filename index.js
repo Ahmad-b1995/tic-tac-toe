82402aa1e457bfda2fae5efc6b8201212d239d0b
ahmad <ahmad.b1995@gmail.com>
2022-07-31 16:08:20 +0430

    done

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const PLAYER_X = "X";
 const PLAYER_O = "O";
 let turn = PLAYER_X;
+
 const tiles = document.querySelectorAll(".tile");
+const boardState = Array(tiles.length);
+const strike = document.getElementById("strike");
+const gameoverArea = document.getElementById("gameover-area");
+
+boardState.fill(null);
 tiles.forEach((tile) => tile.addEventListener("click", tileClick));
 
 setHoverText();
@@ -15,22 +21,68 @@ function setHoverText() {
   const hoverClass = `${turn.toLowerCase()}-hover`;
 
   tiles.forEach((tile) => {
-    console.log(tile.innerText)
-    if(tile.innerText == ''){
+    if (tile.innerText == "") {
       tile.classList.add(hoverClass);
     }
   });
 }
 
 function tileClick() {
+  if (gameoverArea.innerText != "") return;
   const tile = event.target;
-  if (tile.innerText != '') return;
+  const tileNumber = tile.dataset.index;
+  if (tile.innerText != "") return;
   if (turn === PLAYER_X) {
     tile.innerText = PLAYER_X;
     turn = PLAYER_O;
+    boardState[tileNumber - 1] = PLAYER_X;
   } else {
     tile.innerText = PLAYER_O;
+    boardState[tileNumber - 1] = PLAYER_O;
     turn = PLAYER_X;
   }
+  checkWinner();
   setHoverText();
 }
+
+function checkWinner() {
+  //Check for a winner
+  for (const winningCombination of winningCombinations) {
+    const { combo, strikeClass } = winningCombination;
+    const tileValue1 = boardState[combo[0] - 1];
+    const tileValue2 = boardState[combo[1] - 1];
+    const tileValue3 = boardState[combo[2] - 1];
+    const condition =
+      tileValue1 != null &&
+      tileValue1 === tileValue2 &&
+      tileValue1 === tileValue3;
+    console.log(tileValue1, tileValue2, tileValue3);
+    if (condition) {
+      strike.classList.add(strikeClass);
+      gemeoverScreen(tileValue1);
+    }
+  }
+  const allTilesFilledIn = boardState.every((tile) => tile != null);
+  if (allTilesFilledIn) gemeoverScreen();
+}
+
+function gemeoverScreen(winner) {
+  if (winner) {
+    gameoverArea.classList = "visible";
+    gameoverArea.innerText = `player ${winner} won`;
+  } else {
+    gameoverArea.classList = "visible";
+    gameoverArea.innerText = `it's a tie`;
+  }
+}
+
+const winningCombinations = [
+  { combo: [1, 2, 3], strikeClass: "strike-row-1" },
+  { combo: [4, 5, 6], strikeClass: "strike-row-2" },
+  { combo: [7, 8, 9], strikeClass: "strike-row-3" },
+  { combo: [1, 4, 7], strikeClass: "strike-column-1" },
+  { combo: [2, 5, 8], strikeClass: "strike-column-2" },
+  { combo: [3, 6, 9], strikeClass: "strike-column-3" },
+  { combo: [1, 5, 9], strikeClass: "strike-diagonal-1" },
+  { combo: [3, 5, 7], strikeClass: "strike-diagonal-2" },
+];
